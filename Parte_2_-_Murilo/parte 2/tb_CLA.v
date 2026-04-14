8530fca86b042f1621dc32c7b71128be9c14c2f7
MurilodeBrito123 <mrilobrito@gmail.com>
2026-04-14 09:12:55 -0300

    troca_nomes

---
+++ Parte_2_-_Murilo/parte 2/tb_CLA.v
@@ -0,0 +1,43 @@
+module tb_cla_16;
+    reg [15:0] a, b;
+    reg cin;
+    wire [15:0] sum;
+    wire cout, over_under;
+    reg [16:0] expected;
+
+    Cla_16 uut (
+        .a(a),
+        .b(b),
+        .Cin(cin),
+        .sum(sum),
+        .Cout(cout),
+        .overflow(over_under)
+    );
+
+    task testa;
+        begin
+            #1;
+            expected = a + b + cin;
+            if ({cout, sum} !== expected)
+                $display("ERRO: a=%h b=%h cin=%b | Esp=%h Obt=%h", a, b, cin, expected, {cout, sum});
+            else
+                $display("OK: a=%h b=%h cin=%b | Res=%h", a, b, cin, {cout, sum});
+        end
+    endtask
+
+    initial begin
+        $dumpfile("dump.vcd");
+        $dumpvars(0, tb_cla_16);
+        
+        a = 16'h0001; b = 16'h0001; cin = 0; testa();
+        a = 16'hFFFF; b = 16'h0001; cin = 0; testa();
+        a = 16'hAAAA; b = 16'h5555; cin = 0; testa();
+        
+        repeat (5) begin
+            a = $random; b = $random; cin = $random % 2;
+            testa();
+        end
+        
+        $finish;
+    end
+endmodule
